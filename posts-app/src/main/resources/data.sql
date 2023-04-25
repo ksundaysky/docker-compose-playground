13b6c755aaf355855825b6ec1c559e0d8714df04
Krzysztof Niedzielski <krzysztof.niedzielski96@gmail.com>
2023-04-25 11:09:35 +0200

    add postgres support

---
+++ posts-app/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO PostTable (id, authorId, text, postedAt) VALUES
+(1,1,"some text 1","24-04-2023"),
+(2,1,"some text 1","24-04-2023"),
+(3,1,"some text 1","25-04-2023"),
+(4,1,"some text 1","25-04-2023"),
+(5,2,"some text 2","25-04-2023"),
+(6,2,"some text 2","25-04-2023"),
+(7,2,"some text 2","24-04-2023"),
+(8,2,"some text 2","24-04-2023");
---
+++ users-app/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO UserTable (id, authorId, text, postedAt) VALUES
+(1,"jondoe",4),
+(2,"foobar",4);
